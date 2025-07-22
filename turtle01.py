509c1fd42e266a51ee6811c368035c5ff7c41de7
Chaerimy0907 <qkrcofus1026@daum.net>
2025-07-22 10:36:46 +0900

    Update turtle01.py

--- turtle01.py
+++ turtle01.py
@@ -4,10 +4,31 @@ import turtle
 s = turtle.getscreen()
 s.setup(600,600)
 
+# 장애물 생성 거북
+ob=turtle.Turtle()
+ob.shape("square")
+ob.shapesize(6,6)
+
+# 출발/도착 지점 표
+turtle.color("blue")
+turtle.hideturtle()
+turtle.penup()
+turtle.goto(-200,-200)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(200,200)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.showturtle()
+turtle.color("black")
+
+
 # 거북이 변수 지정
-t = turtle.Turtle()
-t.shape("turtle")
-t.penup()
-t.goto(-200,-200)
-t.pendown()
-t.goto(200,200)
+turtle.shape("turtle")
+turtle.penup()
+turtle.goto(-200,-200)
+turtle.pendown()
+turtle.goto(200,200)
+
